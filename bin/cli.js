0e810e797a83efcb84208e11ed6c4dcdd7a1f663
Mostafa Allam <mostafaallam602@gmail.com>
2026-06-19 15:52:25 +0300

    updating the cli.js file to start bpm modeler as excutable project

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 
 import { spawn } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
 
-spawn("npm", ["run", "dev"], {
-  stdio: "inherit",
-  shell: true,
-});
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+spawn(
+  "npm",
+  ["run", "dev"],
+  {
+    cwd: path.join(__dirname, ".."),
+    stdio: "inherit",
+    shell: true,
+  }
+);
